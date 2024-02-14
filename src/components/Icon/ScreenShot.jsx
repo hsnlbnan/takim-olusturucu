d3036b7050338e107f0ed7dbffa607a047234cf9
Hüsnü Lübnan <hsnlbnan@gmail.com>
2024-02-14 23:40:00 +0300

    feat: add more function and refactors

---
+++ src/components/Icon/ScreenShot.jsx
@@ -0,0 +1,18 @@
+import * as React from "react";
+
+const ScreenShotIcon = (props) => (
+  <svg
+    xmlns="http://www.w3.org/2000/svg"
+    xmlSpace="preserve"
+    style={{
+      enableBackground: "new 0 0 30 30",
+    }}
+    viewBox="0 0 30 30"
+    {...props}
+  >
+    <path d="M6 19v-2a1 1 0 0 0-2 0v2a1 1 0 0 0 2 0zM10 5a1 1 0 0 0 1 1h2a1 1 0 0 0 0-2h-2a1 1 0 0 0-1 1zM5 14a1 1 0 0 0 1-1v-2a1 1 0 0 0-2 0v2a1 1 0 0 0 1 1zM23 6h1v1a1 1 0 0 0 2 0V6a2 2 0 0 0-2-2h-1a1 1 0 0 0 0 2zM16 5a1 1 0 0 0 1 1h2a1 1 0 0 0 0-2h-2a1 1 0 0 0-1 1zM7 24H6v-1a1 1 0 0 0-2 0v1a2 2 0 0 0 2 2h1a1 1 0 0 0 0-2zM6 7V6h1a1 1 0 0 0 0-2H6a2 2 0 0 0-2 2v1a1 1 0 0 0 2 0zM24 11v2.001a1 1 0 0 0 2 0V11a1 1 0 0 0-2 0zM25 16h-1.764a2 2 0 0 1-1.789-1.106l-.171-.342a1 1 0 0 0-.894-.552h-4.764a.998.998 0 0 0-.894.553l-.171.342A2.002 2.002 0 0 1 12.764 16H11a1 1 0 0 0-1 1v8a1 1 0 0 0 1 1h14a1 1 0 0 0 1-1v-8a1 1 0 0 0-1-1zm-7 9a4 4 0 1 1 0-8 4 4 0 0 1 0 8z" />
+    <circle cx={18} cy={21} r={2} />
+  </svg>
+);
+
+export default ScreenShotIcon;
--- src/helper/random.js
+++ src/helper/random.js
@@ -1,18 +1,27 @@
-export function randomizeAndSplit(data, chunkSize) {
-  var arrayOfArrays = [];
-  var shuffled = [...data];
-
-  for (let i = shuffled.length - 1; i > 0; i--) {
+function fisherYatesShuffle(array) {
+  for (let i = array.length - 1; i > 0; i--) {
     const j = Math.floor(Math.random() * (i + 1));
-    [shuffled[i], shuffled[j]] = [shuffled[j], shuffled[i]];
+    [array[i], array[j]] = [array[j], array[i]];
+  }
+}
+
+export function randomizeAndSplit(data, chunkSize) {
+  if (!Array.isArray(data)) {
+    throw new Error("First parameter should be an array");
   }
 
-  for (var i = 0; i < shuffled.length; i += chunkSize) {
+  if (typeof chunkSize !== "number") {
+    throw new Error("Second parameter should be a number");
+  }
+
+  const shuffled = [...data];
+
+  fisherYatesShuffle(shuffled);
+
+  const arrayOfArrays = [];
+  for (let i = 0; i < shuffled.length; i += chunkSize) {
     arrayOfArrays.push(shuffled.slice(i, i + chunkSize));
   }
 
-  arrayOfArrays.filter((el) => Object.keys(el).length);
-
-  console.log(arrayOfArrays);
   return arrayOfArrays;
 }
